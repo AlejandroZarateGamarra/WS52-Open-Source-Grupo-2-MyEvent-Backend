56926db9ac504eed12af6090d3bc48b999234ecb
Michael Carmelino Dueñas <carmelino0213@gmail.com>
2024-05-24 21:28:03 -0500

    feat: add routes cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { ProductsComponent } from './products/products.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { ProductdetailsComponent } from './productdetails/productdetails.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {PerfilComponent} from "./perfil/perfil.component";
+import {NopremiumComponent} from "./nopremium/nopremium.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -11,5 +15,8 @@ export const routes: Routes = [
   { path: 'product/:id', component: ProductdetailsComponent },
   { path: 'cart', component: CartPageComponent },
   { path: 'checkout', component: CheckoutComponent },
-  
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'nopremium', component: NopremiumComponent }
 ];
